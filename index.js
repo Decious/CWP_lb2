7f19f0df6f56227383e9c1c1f2c71c365b980855
Decious <Andrey@kaberdin.com>
2021-12-07 11:59:06 +0300

    Task fix

--- index.js
+++ index.js
@@ -11,28 +11,47 @@ app.use(function(req, res, next) {
     next();
   });
 
+app.get('/', function(req,res){
+    try{
+        var data = replaceVariable("ErrorString", "Не понял - ошибка  500 (Internal Server Error)", "error.html");
+        res.setHeader("Content-Type", "text/html").status(500).send(data);
+    }
+    catch (err){
+        console.log(err);
+        res.writeHead(500);
+        res.end(err);
+        return;
+    }
+});
+let loginContent = "Кабердин А.В.";
 app.get('/login/', function (req, res) {
     res.setHeader("Content-type", "text/plain");
-    res.send("Kaberdin A.V.");
+    res.send(loginContent);
 });
 
-app.get('/login/ru', function (req, res) {
-    res.sendFile(path.join(__dirname, 'login.html'));
+app.get('/login/code1', function (req, res) {
+    res.sendFile(getPathToHtml("login.html"));
 });
 
-app.get('/login/by', function (req, res) {
-    fs.readFile('login.html', (err, data) => {
-        if (err) {
-          res.writeHead(500);
-          res.end(err);
-          return;
-        }
-    
-        data = data.toString();
-        res.setHeader("Content-Type","text/plain; charset=UTF-8")
-        res.writeHead(200);
-        res.end(data);
-    });
+app.get('/login/code2', function (req, res) {
+    res.setHeader("Content-Type","text/plain").sendFile(getPathToHtml("login.html"));
+});
+
+app.get('/login/:id', function (req, res) {
+    if(req.params.id == 1){
+        res.writeHeader(200, 
+            { 
+                'Content-Type': 'application/json', 
+                'Access-Control-Allow-Origin': '*', 
+                'X-Author': 'Kaberdin A.V.'
+            });
+            res.write(JSON.stringify(loginContent));
+            res.end();
+            return;
+    }
+    if(req.params.id == 2){
+        res.setHeader("Content-Type", "application/json").send(JSON.stringify(loginContent));
+    }
 });
 
 app.get('/promise/:id', async function (req, res) {
@@ -46,17 +65,16 @@ app.get('/promise/:id', async function (req, res) {
 });
 
 app.get('/fetch/', function (req, res){
-    fs.readFile('fetch.html', (err, data) => {
-        if (err) {
-          res.writeHead(500);
-          res.end(err);
-          return;
-        }
-    
-        data = data.toString().replace(/\{\{port\}\}/, server.address().port);
-        res.writeHead(200);
-        res.end(data, 'utf8');
-    });
+    try{
+        var data = replaceVariable("port",server.address().port,"fetch.html");
+        res.setHeader("Content-Type", "text/html").status(200).send(data);
+    }
+    catch (err){
+        console.log(err);
+        res.writeHead(500);
+        res.end(err);
+        return;
+    }
 });
 
 app.get('/promise/', function(req,res){
@@ -74,7 +92,16 @@ function task(x){
 }
 
 app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, 'error.html'));
+    try{
+        var data = replaceVariable("ErrorString","Ужас - ошибка 404 (не найдено)","error.html");
+        res.setHeader("Content-Type", "text/html").status(404).send(data);
+    }
+    catch (err){
+        console.log(err);
+        res.writeHead(404);
+        res.end(err);
+        return;
+    }
 });
 
 var server = http.createServer(app).listen(process.env.PORT || 8005);
@@ -85,4 +112,17 @@ if(server == undefined){
 else{
     console.log('Node app is working!');
     console.log('listening on: ', server.address().port);
+}
+
+function replaceVariable(variableName, value, htmlFileName){
+    var htmlContent = fs.readFileSync(htmlFileName, 'utf-8');
+
+    var regex = new RegExp("\\{\\{"+variableName+"\\}\\}"); 
+    htmlContent = htmlContent.toString().replace(regex, value);
+    console.log(`RESULT ${htmlContent}`);
+    return htmlContent;
+}
+
+function getPathToHtml(htmlFileName){
+    return path.join(__dirname, `/${htmlFileName}`);
 }
